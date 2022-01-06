4fdf049ef9216f3969380dd440f843b2ff6eae49
Seeky <seek916@gmail.com>
2022-01-05 20:36:21 -0800

    user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const router = require('../routes/api/user-routes');
 
 //create our User models
 class User extends Model {}
@@ -7,7 +8,42 @@ class User extends Model {}
 //define table columns and configurations
 User.init(
     {
-        //TABLE COLUMN DEFINITIONS GO HERE
+        //define an id column
+        id: {
+            //use special Sequelize DataTypes objust provide what type of data it is
+        type: DataTypes.INTEGER,
+        //this is the equivalent of SQL's NOT NULL option
+        allowNull: false,
+        //instruct that this is the Primary Key
+        primaryKey: true,
+        //turn on auto-increment
+        autoIncrement: true    
+        },
+    //define a username column
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    //define an email column
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        //there cannot be any duplicate email valuies in this TABLe
+        unique: true,
+        //if allowNull is set to false, we can run ouir data through validator
+        validate: {
+            isEmail: true
+        }
+    },
+    //define a password column 
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            //this means the password must be at least four characters long
+            len: [4]
+        }
+    }
     },
     {
         //TABLE CONFIGURATION OPTIONS GO HERE 
@@ -23,6 +59,15 @@ User.init(
     modelName: 'user'
   }
 );
+
+//GET /api/users
+
+
+//GET /api/users/1
+
+
+//POST /api/users
+
 
 module.exports = User;
     
