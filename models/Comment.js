d5e146d422def3710a1ac7d5a162047990eee739
Seeky <seek916@gmail.com>
2022-01-09 14:57:53 -0800

    heroku set

---
+++ models/Comment.js
@@ -0,0 +1,17 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    // columns here
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+}
+);
+
+module.exports = Comment;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { sequelize } = require('../../config/connection');
-const { Post, User } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 //get all users
 router.get('/', (req,res) => {
@@ -12,6 +12,14 @@ router.get('/', (req,res) => {
     order: [['created_at', 'DESC']],
         include: [
             {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+                {
                 model: User,
                 attributes:['username']
             }
